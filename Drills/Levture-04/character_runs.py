c67fffc732eeef37185946ca8a98bc2f6572c4e5
Kyulestial(MSI) <clsrn863@naver.com>
2018-09-13 18:50:07 +0900

    Drill-04 Complete
    2017180003 김민규
    https://www.youtube.com/watch?v=5T4v_hd5hc4&feature=youtu.be

--- Drills/Levture-04/character_runs.py
+++ Drills/Levture-04/character_runs.py
@@ -1,12 +1,31 @@
 from pico2d import *
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('run_animation.png')
+character = load_image('animation_sheet.png')
 
 x = 0
+frame = 0
+run_switch = 0
 
-# 여기를 채우세요.
+while(1):
+    clear_canvas()
+    grass.draw(400,30)
+    frame = (frame + 1) % 8
 
+    if(run_switch == 0):
+        character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+        update_canvas()
+        x += 10
+        if(x >= 760):
+            run_switch = 1
+    elif(run_switch == 1):
+        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+        update_canvas()
+        x -= 10
+        if(x <= 20):
+            run_switch = 0
+    delay(0.05)
+    get_events()
 
 close_canvas()
 
