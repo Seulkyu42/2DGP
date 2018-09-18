bc16c38bb5a16e4bf72db7e798ff8f9cac99b6ab
Kyulestial(MSI) <clsrn863@naver.com>
2018-09-18 19:09:07 +0900

    Real Drill_05 Reset
    Codes are so Messy
    commit : 7

--- Drills/Drills-05/Drill_05.py
+++ Drills/Drills-05/Drill_05.py
@@ -6,35 +6,18 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-def calculate(x1,y1,x2,y2):
-    fx,fy =  (x2 - x1), (y2 - y1)
-
-    if (fx > 0):
-        switch = 1
-        return switch
-
-    elif(fx < 0):
-        switch = 2
-        return switch
-
-
-
-
-
-
 def move_locate(x1,y1,x2,y2):
     clear_canvas()
-    grass.draw(400,30)
-    character.draw(x1,y1)
+    grass.draw_now(400,30)
+    character.draw_now(x1, y1)
 
-    calculate(x1,y1,x2,y2)
+    calculate(203,535,132,243)
 
 
-    update_canvas()
     delay(0.01)
 
 while True:
-    move_locate(203,535 , 132,243)
-    move_locate(132,243 , 535,470)
+    move_locate(203,535,132,243)
+
 
 close_canvas()
