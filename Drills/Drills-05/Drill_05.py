ecf46cc4059827797ce0458439218300987e598b
Kyulestial(MSI) <clsrn863@naver.com>
2018-09-18 18:49:23 +0900

    Real Drill_05 Input_data
    Input Location data
    commit : 4

--- Drills/Drills-05/Drill_05.py
+++ Drills/Drills-05/Drill_05.py
@@ -10,11 +10,13 @@ def move_locate(x,y):
     clear_canvas()
     grass.draw(400,30)
     character.draw(x,y)
-
+    
     update_canvas()
     delay(0.01)
 
 while True:
     move_locate(203,535)
+    move_locate(132,243)
+    move_locate(535,470)
 
 close_canvas()
