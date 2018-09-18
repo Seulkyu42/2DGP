e8accb23b02138dee86b2e195dcba4aa1d2f6e72
Kyulestial(MSI) <clsrn863@naver.com>
2018-09-18 19:35:24 +0900

    Real Drill_05 Input_Data_2
    Input_Data 2!
    commit : 8

--- Drills/Drills-05/Drill_05.py
+++ Drills/Drills-05/Drill_05.py
@@ -11,13 +11,17 @@ def move_locate(x1,y1,x2,y2):
     grass.draw_now(400,30)
     character.draw_now(x1, y1)
 
-    calculate(203,535,132,243)
-
-
     delay(0.01)
 
 while True:
-    move_locate(203,535,132,243)
-
+    move_locate(203, 535, 132, 243)
+    move_locate(132, 243, 535, 470)
+    move_locate(535, 470, 477, 203)
+    move_locate(477, 203, 715, 136)
+    move_locate(715, 136, 316, 225)
+    move_locate(316, 225, 510, 92)
+    move_locate(510, 92, 692, 518)
+    move_locate(692, 518, 682, 336)
+    move_locate(682, 336, 712, 349)
 
 close_canvas()
