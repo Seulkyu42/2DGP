c53a176a8127ef18da6e38a4f8de20fa5f274c2c
Kyulestial(MSI) <clsrn863@naver.com>
2018-09-18 18:54:21 +0900

    Real Drill_05 Input_data
    Calculate Points
    commit : 5

--- Drills/Drills-05/Drill_05.py
+++ Drills/Drills-05/Drill_05.py
@@ -6,17 +6,21 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-def move_locate(x,y):
+def calculate(x1,y1,x2,y2)
+
+def move_locate(x1,y1,x2,y2):
     clear_canvas()
     grass.draw(400,30)
-    character.draw(x,y)
-    
+    character.draw(x1,y1)
+
+    calculate(x1,y1,x2,y2)
+
+
     update_canvas()
     delay(0.01)
 
 while True:
-    move_locate(203,535)
-    move_locate(132,243)
-    move_locate(535,470)
+    move_locate(203,535 , 132,243)
+    move_locate(132,243 , 535,470)
 
 close_canvas()
