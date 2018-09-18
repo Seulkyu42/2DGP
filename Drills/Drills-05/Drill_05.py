d67dab7ed8a50efcb56ae427eeb58a4ee6a82f3e
Kyulestial(MSI) <clsrn863@naver.com>
2018-09-18 19:53:28 +0900

    Real Drill_05 remove_Calculate()
    Remove Data in Calculate Function
    and Move to move_locate()
    
    Commit : 9

--- Drills/Drills-05/Drill_05.py
+++ Drills/Drills-05/Drill_05.py
@@ -6,17 +6,22 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-def calculate(x1,y1,x2,y2):
-    
-
 def move_locate(x1,y1,x2,y2):
     clear_canvas()
-    grass.draw_now(400,30)
-    character.draw_now(x1, y1)
+    grass.draw(400,30)
 
-    calculate(x1,y1,x2,y2)
+    fx = (x2 - x1) / 30
+    fy = (y2 - y1) / 30
 
-    delay(0.01)
+    for i in range(1,30):
+        clear_canvas()
+        grass.draw(400, 30)
+        i += 1
+        character.draw(x1 + fx * i, y1+ fy * i)
+        update_canvas()
+        delay(0.01)
+
+    get_events()
 
 while True:
     move_locate(203, 535, 132, 243)
@@ -28,5 +33,6 @@ while True:
     move_locate(510, 92, 692, 518)
     move_locate(692, 518, 682, 336)
     move_locate(682, 336, 712, 349)
+    move_locate(712, 349, 203, 535)
 
 close_canvas()
