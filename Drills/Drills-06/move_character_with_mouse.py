6e4e0f831bf14e994e4024e034bbabc516f06e27
Kyulestial(MSI) <clsrn863@naver.com>
2018-09-20 22:37:11 +0900

    Drill_06
    commit ; 8

--- Drills/Drills-06/move_character_with_mouse.py
+++ Drills/Drills-06/move_character_with_mouse.py
@@ -2,9 +2,6 @@ from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-
-
-
 def handle_events():
     global running
     global x,y,x1,y1,x2,y2
@@ -17,13 +14,12 @@ def handle_events():
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
-
             x1,y1 = x2 ,y2
             x2, y2 = event.x, KPU_HEIGHT - 1 - event.y
 
             fx = (x1 - x2) / 30
             fy = (y1 - y2) / 30
-
+            i = 0
 
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
@@ -43,21 +39,19 @@ fx,fy = 0,0
 frame = 0
 hide_cursor()
 
-def move():
-    for i in range(1, 30):
-        i += 1
-        if (fx * 30 < 0):
-            character.clip_draw(frame * 100, 100, 100, 100, x2 + fx * i, y2 + fy * i)
-        if (fx * 30 > 0):
-            character.clip_draw(frame * 100, 0, 100, 100, x2 + fx * i, y2 + fy * i)
-
+i = 0
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     cursor.draw(x, y)
+    if (fx * 30 < 0):
+        character.clip_draw(frame * 100, 100, 100, 100, x1 + fx, y1 + fy)
+    elif (fx * 30 > 0):
+        character.clip_draw(frame * 100, 0, 100, 100, x1 + fx, y1 + fy)
+    fx += fx
+    fy += fy
     update_canvas()
-    move()
     update_canvas()
     get_events()
     frame = (frame + 1) % 8
