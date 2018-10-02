f4a750cfee06e34754fe88b5fd1e2e9730e2c05f
Kyulestial(MSI) <clsrn863@naver.com>
2018-10-02 18:46:38 +0900

    Drill_07 Parametric representation
    x = (1 - t)*p1[0]+t*p2[0]
     y = (1 - t)*p1[1]+t*p2[1]
    Commit : 4

--- Drills/Drills-07/drill_07.py
+++ Drills/Drills-07/drill_07.py
@@ -4,7 +4,6 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
     global running
-
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -12,12 +11,28 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+frame = 0
+def draw_line(p1,p2):
+    global frame
+    for i in range(0,300 + 1, 2):
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        t = i / 300
+
+        x = (1 - t)*p1[0]+t*p2[0]
+        y = (1 - t)*p1[1]+t*p2[1]
+
+        fx = (p1[0] - p2[0])
+
+
+
+        update_canvas()
+
 open_canvas(KPU_WIDTH,KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
 running = True
-frame = 0
 i = 0
 
 while running:
