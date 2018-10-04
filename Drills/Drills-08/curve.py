6f2be0bfd22afd8a0c512e41082bc340eaef7caf
Kyulestial(MSI) <clsrn863@naver.com>
2018-10-04 18:32:23 +0900

    Drill_08_1 Y_Location Fix
    Commit : 5

--- Drills/Drills-08/curve.py
+++ Drills/Drills-08/curve.py
@@ -118,18 +118,16 @@ def draw_curve_4_Drills(p1, p2, p3, p4):
         draw_point((x, y))
     draw_point(p4)
 
-    for i in range(0, 25, 2):
-        t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p3[0] + (3*t**3 - 5*t**2 + 2)*p4[0] + (-3*t**3 + 4*t**2 + t)*p1[0] + (t**3 - t**2)*p2[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p3[1] + (3*t**3 - 5*t**2 + 2)*p4[1] + (-3*t**3 + 4*t**2 + t)*p1[1] + (t**3 - t**2)*p2[1])/2
-        draw_point((x, y))
-    draw_point(p3)
-
     # draw p4-p1
-    for i in range(50, 100, 2):
+    for i in range(42, 100, 2):
         t = i / 100
         x = (2*t**3-3*t+1)*p3[0]+(-4*t**2+4*t)*p4[0]+(2*t**2-t)*p1[0]
         y = (2*t**3-3*t+1)*p3[1]+(-4*t**2+4*t)*p4[1]+(2*t**2-t)*p1[1]
+
+        if(i  < 78):
+            y = y - 17
+        if( i == 76):
+            draw_point((x - 0.5, y+17))
         draw_point((x, y))
     draw_point(p1)
 
