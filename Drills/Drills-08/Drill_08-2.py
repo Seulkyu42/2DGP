15dd7a81e4579bed745cc3c2a08f69bb73fc3f9d
Kyulestial(MSI) <clsrn863@naver.com>
2018-10-04 18:46:54 +0900

    Drill_08_2 Skeleton
    Commit : 2

---
+++ Drills/Drills-08/Drill_08-2.py
@@ -0,0 +1,33 @@
+from pico2d import *
+import random
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+
+open_canvas(KPU_WIDTH,KPU_HEIGHT)
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+
+running = True
+
+
+while running:
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+
+    update_canvas()
+    get_events()
+
+    handle_events()
+
+close_canvas()
