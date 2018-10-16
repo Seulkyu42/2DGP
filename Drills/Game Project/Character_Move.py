0af9b3009df6f013b3ee1a45aaa319985d71d75f
Kyulestial(MSI) <clsrn863@naver.com>
2018-10-16 14:31:47 +0900

    Project Move 1/7 Complete
    Wow

--- Drills/Game Project/Character_Move.py
+++ Drills/Game Project/Character_Move.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import os
 
 open_canvas(1600,900)
 
@@ -9,13 +10,16 @@ class Right_Sprite:
     def __init__(self):
         self.x, self.y = 110, 190
         self.frame = 0
-        self.image = load_image('Running Sprite.png')
+        os.chdir("C:\\Users\\김민규\\Documents\\Github\\2DGP\\Drills\\Game Project\\Resources")
+        self.image = load_image('Right_run.png')
 
     def run(self):
-        self.image.clip_draw(self.frame * 105, 900, 100, 190, self.x, self.y)
+        self.image.clip_draw(self.frame * 110, 0, 110, 190, self.x, self.y)
 
     def update(self):
         pass
+
+
 def enter():
     global Right
     Right = Right_Sprite()
@@ -30,10 +34,14 @@ def handle_events():
             pass
         if event.key == SDLK_RIGHT:
             RunSwitch = True
-
-        if (RunSwitch == True):
             Right.frame = (Right.frame + 1) % 6
             Right.x += 15
+
+        if event.key == SDLK_RIGHT:
+            RunSwitch = True
+            Right.frame = (Right.frame + 1) % 6
+            Right.x += 15
+
 
 
 enter()
