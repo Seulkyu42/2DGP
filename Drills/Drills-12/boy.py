f9267cc89b64b2daed5362f3d39d7072a7fd3b17
Kyulestial(MSI) <clsrn863@naver.com>
2018-10-30 17:31:23 +0900

    Drill_12 Copy
    :100:

---
+++ Drills/Drills-12/boy.py
@@ -0,0 +1,233 @@
+import game_framework
+from pico2d import *
+from ball import Ball
+import math
+import random
+import game_world
+
+# Boy Run Speed
+# fill expressions correctly
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 20.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+# Boy Action Speed
+# fill expressions correctly
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+
+# Boy Event
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER, SPACE, Ghost = range(7)
+
+key_event_table = {
+    (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
+    (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
+    (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
+    (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
+    (SDL_KEYDOWN, SDLK_SPACE): SPACE
+}
+
+Idle_Time = 0.0
+
+# Boy States
+
+class IdleState:
+    @staticmethod
+    def enter(boy, event):
+        global Idle_Time
+        Idle_Time = get_time()
+        if event == RIGHT_DOWN:
+            boy.velocity += RUN_SPEED_PPS
+        elif event == LEFT_DOWN:
+            boy.velocity -= RUN_SPEED_PPS
+        elif event == RIGHT_UP:
+            boy.velocity -= RUN_SPEED_PPS
+        elif event == LEFT_UP:
+            boy.velocity += RUN_SPEED_PPS
+
+
+    @staticmethod
+    def exit(boy, event):
+        if event == SPACE:
+            boy.fire_ball()
+        pass
+
+    @staticmethod
+    def do(boy):
+        global Idle_Time
+        boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+        if get_time() - Idle_Time >= 1:
+            boy.save_x = boy.x
+            boy.save_y = boy.y
+            boy.add_event(SLEEP_TIMER)
+
+    @staticmethod
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_draw(int(boy.frame) * 100, 300, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(int(boy.frame) * 100, 200, 100, 100, boy.x, boy.y)
+
+
+class RunState:
+
+    @staticmethod
+    def enter(boy, event):
+        global Idle_Time
+        if event == RIGHT_DOWN:
+            boy.velocity += RUN_SPEED_PPS
+        elif event == LEFT_DOWN:
+            boy.velocity -= RUN_SPEED_PPS
+        elif event == RIGHT_UP:
+            Idle_Time = 0.0
+            boy.velocity -= RUN_SPEED_PPS
+        elif event == LEFT_UP:
+            Idle_Time = 0.0
+            boy.velocity += RUN_SPEED_PPS
+
+        boy.dir = clamp(-1,boy.velocity, 1)
+
+    @staticmethod
+    def exit(boy, event):
+        if event == SPACE:
+            boy.fire_ball()
+
+    @staticmethod
+    def do(boy):
+        boy.save_x = boy.x
+        boy.save_y = boy.y
+        boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+        boy.x += boy.velocity * game_framework.frame_time
+        boy.x = clamp(25, boy.x, 1600 - 25)
+
+    @staticmethod
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_draw(int(boy.frame) * 100, 100, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(int(boy.frame) * 100, 0, 100, 100, boy.x, boy.y)
+
+class SleepState:
+
+    @staticmethod
+    def enter(boy, event):
+        global Idle_Time
+        Idle_Time = get_time()
+        boy.frame = 0
+        boy.sp = 0
+
+    @staticmethod
+    def exit(boy, event):
+        pass
+
+    @staticmethod
+    def do(boy):
+        global Idle_Time
+        boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+        boy.sp += 1
+        if get_time() - Idle_Time >= 1:
+            boy.save_x = boy.x
+            boy.save_y = boy.y
+            boy.add_event(Ghost)
+
+    @staticmethod
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_composite_draw(int(boy.frame) * 100, 300, 100, 100, 3.141592 / 2 - boy.sp / 45, '', boy.save_x-15, boy.save_y, 100, 100)
+            boy.image.opacify(1)
+            boy.image.clip_composite_draw(int(boy.frame) * 100, 300, 100, 100, 3.141592 / 2, '', boy.save_x - 25, boy.save_y - 25, 100, 100)
+            boy.image.opacify(0.5)
+
+        else:
+            boy.image.clip_composite_draw(int(boy.frame) * 100, 200, 100, 100, -3.141592 / 2 + boy.sp / 45, '', boy.save_x  +  15, boy.save_y , 100, 100)
+            boy.image.opacify(1)
+            boy.image.clip_composite_draw(int(boy.frame) * 100, 200, 100, 100, -3.141592 / 2, '', boy.save_x + 25, boy.save_y - 25, 100, 100)
+            boy.image.opacify(0.5)
+
+class GhostState:
+    @staticmethod
+    def enter(boy, event):
+        boy.x= 0
+        boy.y = 0
+
+    @staticmethod
+    def exit(boy, event):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.x = 300 * math.sin(boy.angle * 3.14 / 360)
+        boy.y = 300 + 300 * -math.cos(boy.angle * 3.14 / 360)
+        boy.angle += 1440 * game_framework.frame_time
+
+        boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+
+
+    @staticmethod
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_draw(int(boy.frame) * 100, 300, 100, 100, boy.save_x + boy.x, boy.save_y + boy.y)
+            boy.image.opacify(1)
+            boy.image.clip_composite_draw(int(boy.frame) * 100, 300, 100, 100, 3.141592 / 2, '', boy.save_x - 25, boy.save_y - 25, 100, 100)
+            boy.image.opacify(0.5)
+
+        else:
+            boy.image.clip_draw(int(boy.frame) * 100, 200, 100, 100, boy.save_x + boy.x, boy.save_y + boy.y)
+            boy.image.opacify(1)
+            boy.image.clip_composite_draw(int(boy.frame) * 100, 200, 100, 100, -3.141592 / 2, '', boy.save_x + 25, boy.save_y - 25, 100, 100)
+            boy.image.opacify(0.5)
+
+next_state_table = {
+    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState, SLEEP_TIMER: SleepState ,SPACE: IdleState, Ghost : GhostState},
+    RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState, LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState, SPACE: RunState, Ghost : GhostState},
+    SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState, LEFT_UP: RunState, RIGHT_UP: RunState, SPACE: IdleState, Ghost : GhostState},
+    GhostState : {LEFT_DOWN: GhostState, RIGHT_DOWN: GhostState, LEFT_UP: GhostState, RIGHT_UP: GhostState, SPACE: GhostState, Ghost : GhostState}
+}
+
+class Boy:
+
+    def __init__(self):
+        self.x, self.y = 1600 // 2, 90
+        # Boy is only once created, so instance image loading is fine
+        self.image = load_image('animation_sheet.png')
+        self.font = load_font('ENCR10B.TTF',16)
+        self.dir = 1
+        self.velocity = 0
+        self.frame = 0
+        self.event_que = []
+        self.cur_state = IdleState
+        self.cur_state.enter(self, None)
+        self.save_x = self.x
+        self.save_y = self.y
+        self.angle = 0
+        self.sp = 0
+
+    def fire_ball(self):
+        ball = Ball(self.x, self.y, self.dir*3)
+        game_world.add_object(ball, 1)
+
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
+
+    def update(self):
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state = next_state_table[self.cur_state][event]
+            self.cur_state.enter(self, event)
+
+    def draw(self):
+        self.cur_state.draw(self)
+        self.font.draw(self.save_x - 60, self.save_y + 50, '(Time: %3.2f)' % get_time(), (255,255,0))
+
+    def handle_event(self, event):
+        if (event.type, event.key) in key_event_table:
+            key_event = key_event_table[(event.type, event.key)]
+            self.add_event(key_event)
+
