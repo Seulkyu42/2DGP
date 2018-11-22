82714600bd4377e16e2bdfa039e4d0aa7c6ba13a
Kyulestial(MSI) <clsrn863@naver.com>
2018-11-22 18:43:04 +0900

    Update ball.py
    commit : 6

--- Drills/Drills-15/ball.py
+++ Drills/Drills-15/ball.py
@@ -12,16 +12,11 @@ class Ball:
             Ball.image = load_image('ball21x21.png')
         self.x, self.y= random.randint(500,2100), random.randint(350,1400)
 
-    def set_background(self, bg):
-        self.bg = bg
-        self.x = self.bg.w / 2
-        self.y = self.bg.h / 2
-
     def get_bb(self):
         return self.x -main_state.boy.x  - 10, self.y -main_state.boy.y - 10, self.x -main_state.boy.x + 10, self.y -main_state.boy.y + 10
 
     def draw(self):
-        self.image.draw(self.x-main_state.boy.x,self.y-main_state.boy.y)
+        self.image.draw(self.x-main_state.boy.x, self.y-main_state.boy.y)
         draw_rectangle(*self.get_bb())
 
     def update(self):
